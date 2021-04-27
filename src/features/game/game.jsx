008bc8288bfc3ec66e546e580ed303d95d30219d
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-27 00:57:27 -0400

    add basic Game layout

--- src/features/game/game.jsx
+++ src/features/game/game.jsx
@@ -1,11 +1,107 @@
 import React from "react"
-import { Box, Heading } from "grommet"
+import {
+    Box,
+    Heading,
+    Text,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Button,
+} from "grommet"
+import api from "../../utils"
+
+const initDirections = {
+    n: true,
+    e: true,
+    s: true,
+    w: true,
+}
 
 function Game() {
+    const [room, setRoom] = React.useState(null)
+    const [error, setError] = React.useState(null)
+    const [valid, setValid] = React.useState(initDirections)
+
+    React.useEffect(() => {
+        api.initialize()
+            .then(res => {
+                setRoom({ ...res.data })
+            })
+            .catch(err => setError(err.message))
+    }, [])
+
+    function handleMove(direction) {
+        api.move(direction)
+            .then(res => {
+                if (res.data.error_msg) {
+                    setError(res.data.error_msg)
+                    setValid(prev => ({ ...prev, [direction]: false }))
+                } else {
+                    setRoom(res.data)
+                    setValid(initDirections)
+                    setError(null)
+                }
+            })
+            .catch(err => setError(err.message))
+    }
+
     return (
         <Box fill align="center" direction="column" justify="center">
             <Box width="large">
                 <Heading>Game</Heading>
+                {room && (
+                    <Card
+                        height="medium"
+                        width="large"
+                        background="dark-2"
+                        justify="center"
+                        align="left"
+                        pad="large"
+                    >
+                        <CardHeader>
+                            <Heading>{room.title}</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text size="large" weight="bold">
+                                {room.description}
+                            </Text>
+                            <Text>
+                                Other Players:
+                                <br />
+                                {room.players.length > 0
+                                    ? room.players.map(player => (
+                                          <p key={player.name}>{player.name}</p>
+                                      ))
+                                    : "None"}
+                            </Text>
+                        </CardBody>
+                        <CardFooter>
+                            <Box width="large" direction="row" justify="evenly">
+                                {Object.entries(valid).map(([dir, isValid]) => {
+                                    return (
+                                        <Button
+                                            key={dir}
+                                            fill="horizontal"
+                                            primary
+                                            onClick={() => handleMove(dir)}
+                                            disabled={!isValid}
+                                        >
+                                            {dir}
+                                        </Button>
+                                    )
+                                })}
+                            </Box>
+                        </CardFooter>
+                    </Card>
+                )}
+                {error && (
+                    <Text color="status-critical" textAlign="center">
+                        <strong>There's been an error!</strong>
+                        <br />
+                        {error}
+                    </Text>
+                )}
             </Box>
         </Box>
     )
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,11 @@ import axios from "axios"
 const API_BASE_URL = "http://127.0.0.1:8000"
 
 const axiosAuth = function () {
-    const token = window.localStorage.getItem("token")
+    const token = window.localStorage.getItem("key")
 
     return axios.create({
         headers: {
-            Authorization: JSON.parse(token),
+            Authorization: `Token ${token}`,
         },
         baseURL: API_BASE_URL,
     })
@@ -17,13 +17,12 @@ export const api = {
     API_BASE_URL,
     axios: axios.create({ baseURL: API_BASE_URL }),
     axiosAuth,
+    login: userInfo => axios.post(`${API_BASE_URL}/api/api-login`, userInfo),
+    register: userInfo =>
+        axios.post(`${API_BASE_URL}/api/api-register`, userInfo),
+    initialize: () => axiosAuth().get("/api/adv/init"),
+    move: direction => axiosAuth().post("/api/adv/move", { direction }),
+    say: message => axiosAuth().post("/api/adv/say", message),
 }
-
-api.login = userInfo => axios.post(`${API_BASE_URL}/api/api-login`, userInfo)
-api.register = userInfo =>
-    axios.post(`${API_BASE_URL}/api/api-register`, userInfo)
-api.initialize = () => axiosAuth().get("/api/adv/init")
-api.move = direction => axiosAuth().post("/api/adv/move", { direction })
-api.say = message => axiosAuth().post("/api/adv/say", message)
 
 export default api
